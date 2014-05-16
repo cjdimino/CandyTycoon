0503825d4c52669a592d7f376120ee2259f916b5
Cj Dimino <cj.dimino@me.com>
2014-05-15 21:28:26 -0400

    Added lwjgl to buildpath

---
+++ CandyTycoon/src/exec/Test.java
@@ -0,0 +1,18 @@
+package exec;
+
+import game.Game;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Game game = new Game();
+		
+
+	}
+
+}
